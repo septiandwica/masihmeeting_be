84f4e40922d01852eaf0ab165786eb10648dfdec
sqizzo <baihaqimnb@gmail.com>
2025-08-01 10:21:18 +0700

    fix: fixed routing

--- routes/transcription.js
+++ routes/transcription.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { transcribeYouTube } = require("../controller/transcriptionController");
+const {
+  transcribeYouTube,
+  transcribeVideo,
+  transcribeAudio,
+} = require("../controller/transcriptionController");
 const { isLoggedIn } = require("../middlewares/authMiddlewares");
 
 /**
@@ -9,5 +13,19 @@ const { isLoggedIn } = require("../middlewares/authMiddlewares");
  * @access  Private
  */
 router.post("/youtube", isLoggedIn, transcribeYouTube);
+
+/**
+ * @route   POST /transcribe/video
+ * @desc    Transcribe mp4 ke backend python
+ * @access  Private
+ */
+router.post("/video", isLoggedIn, transcribeVideo);
+
+/**
+ * @route   POST /transcribe/audio
+ * @desc    Transcribe mp3 ke backend python
+ * @access  Private
+ */
+router.post("/audio", isLoggedIn, transcribeAudio);
 
 module.exports = router;
